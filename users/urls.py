27699e5ac70dbb789e8350b76488d2f52dd95484
LudaKend <Ludakend@gmail.com>
2024-01-29 10:53:55 +0300

    CRUD для пользователей в приложении users

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import RegisterView, UserLoginView
+from users.views import RegisterView, UserLoginView, UserListView, UserDetailView, UserUpdateView, UserDeleteView
 
 app_name = UsersConfig.name
 
@@ -9,4 +9,8 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(template_name='users/login.html'), name='route_login_users'),
     path('logout/', LogoutView.as_view(), name='route_logout_users'),
     path('register/', RegisterView.as_view(template_name='users/register.html'), name='route_register_users'),
+    path('list_view/', UserListView.as_view(), name='route_users_list'),
+    path('view/<int:pk>/', UserDetailView.as_view(), name='route_users_view'),
+    path('update/<int:pk>/', UserUpdateView.as_view(), name='route_users_update'),
+    path('delete/<int:pk>/', UserDeleteView.as_view(), name='route_users_delete'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,17 +1,41 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from users.models import User
 from django.contrib.auth.views import LoginView
 from users.forms import UserRegisterForm, UserLoginForm
 
 
 class RegisterView(CreateView):
-    '''контроллер для регистрации пользователя'''
+    '''контроллер для регистрации пользователя-спаммера'''
     model = User
     form_class = UserRegisterForm
 
 
 class UserLoginView(LoginView):
-    '''контроллер, чтобы залогиниться пользователю'''
+    '''контроллер, чтобы залогиниться пользователю-спаммеру'''
     model = User
     form_class = UserLoginForm
+
+class UserListView(ListView):
+    '''класс-контроллер для просмотра списка пользователей-спаммеров, работающий с шаблоном user_list.html'''
+    model = User
+    extra_context = {'name_page': 'Список пользователей-спаммеров'}
+
+
+class UserDetailView(DetailView):
+    '''класс-контроллер для просмотра пользователя-спаммера, работающий с шаблоном user_detail.html'''
+    model = User
+    extra_context = {'name_page': 'Карточка пользователя-спаммера'}
+
+
+class UserUpdateView(UpdateView):
+    '''класс-контроллер для изменения пользователя-спаммера,работающий с шаблоном user_form.html'''
+    model = User
+    extra_context = {'name_page': 'Изменение пользователя-спаммера'}
+
+
+class UserDeleteView(DeleteView):
+    '''класс-контроллер для удаления пользователя-спаммера,работающий с шаблоном user_confirm_delete.html'''
+    model = User
+    extra_context = {'name_page': 'Удаление пользователя-спаммера'}
+
